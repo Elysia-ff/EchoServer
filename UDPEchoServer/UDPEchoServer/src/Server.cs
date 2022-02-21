ed4653a8346c52258f8729671b9a5832dd1028ed
Elysia-ff <jhk19960@gmail.com>
2022-02-22 00:07:31 +0900

    add udp echo server

---
+++ UDPEchoServer/UDPEchoServer/src/Server.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPEchoServer
+{
+    public class Server
+    {
+        private const int PORT = 8888;
+
+        private UdpClient udpClient = new UdpClient(PORT);
+
+        public void Start()
+        {
+            Console.WriteLine("[Info] Server started.");
+
+            Task.Run(ReceiveProcess);
+        }
+
+        private void ReceiveProcess()
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = udpClient.Receive(ref ipEndPoint);
+
+                    udpClient.Send(buffer, buffer.Length, ipEndPoint);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[Error] {e}");
+                }
+            }
+        }
+    }
+}
